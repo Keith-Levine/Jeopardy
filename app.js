5c8a8d6b0c374ba7d64f2f4cee652d00d9ca5d77
Keith Levine <Levine.Keith@gmail.com>
2021-07-26 10:28:20 -0700

    still not able to sue api due to cors error

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hi')
+const category1 = document.querySelector('.category-1')
+const category2 = document.querySelector('.category-2')
+const category3 = document.querySelector('.category-3')
+const category4 = document.querySelector('.category-4')
+const category5 = document.querySelector('.category-5')
+
+let baseURL = 'http://jservice.io/api/random'
+
+const getQuestion = () => {
+    fetch(baseURL)
+    .then(res => console.log(res))
+}
